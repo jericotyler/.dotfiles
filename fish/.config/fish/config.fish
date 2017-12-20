61bc4d4180d25c530ef7b43be9a315b8748ff15b
Jeremy T Swink <jeremytswink@gmail.com>
2017-12-20 05:42:22 -0500

    Getting ready to just completely fuck up my system

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,8 @@
 set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/powerline/bindings/fish"
 powerline-setup
 # Start X at login
-if status --is-login
-    if test -z "$DISPLAY" -a $XDG_VTNR = 1
-        exec startx -- -keeptty
-    end
-end
+#if status --is-login
+#    if test -z "$DISPLAY" -a $XDG_VTNR = 1
+#        exec startx -- -keeptty
+#    end
+#end
